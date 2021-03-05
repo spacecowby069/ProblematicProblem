2f7cfdd328abcd24698329122e083efb5184dd28
Michael Farris <spacecowby069@yahoo.com>
2021-03-05 17:18:20 -0500

    My Final Commit

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -1,31 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
 
-ProblematicProblem
+
+namespace ProblematicProblem
 {
-    Program class
-    {
-        Random rng;        
+    public class Program 
+    {        
         static bool cont = true;
-        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" }
+        static List<string> activities = new List<string>() { "Movies, ", "Paintball, ", "Bowling, ", "Lazer Tag, ", "LAN Party, ", "Hiking, ", "Axe Throwing, ", "Wine Tasting", "" };
 
         static void Main(string[] args)
         {
-            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ")
-            bool cont = bool.Parse(Console.ReadLine());
+            Random rng = new Random();
+
+
+            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
+            var contResponse = Console.ReadLine().ToLower();
+            if(contResponse == "yes") 
+            {
+                cont = true;
+            }
+            else
+            {
+                cont = false;
+                Environment.Exit(0);
+            }
 
             Console.WriteLine();
-
+            
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine();
 
             Console.WriteLine();
 
             Console.Write("What is your age? ");
-            int userAge = Console.ReadLine();
+            int userAge = int.Parse(Console.ReadLine());
 
             Console.WriteLine();
 
             Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            bool seeList = bool.Parse(Console.ReadLine());
+            bool seeList;
+            var contResponse2 = Console.ReadLine().ToLower();
+            if (contResponse2 == "sure")
+            {
+                seeList = true;
+            }
+            else
+            {
+                seeList = false;
+            }
 
             if (seeList)
             {
@@ -37,7 +61,17 @@ ProblematicProblem
 
                 Console.WriteLine();
                 Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-                bool addToList = bool.Parse(Console.ReadLine());
+                bool addToList;
+                var contResponse3 = Console.ReadLine().ToLower();
+                if (contResponse3 == "yes")
+                {
+                    addToList = true;
+                }
+                else
+                {
+                    addToList = false;
+                }
+
                 Console.WriteLine();
 
                 while (addToList)
@@ -47,7 +81,7 @@ ProblematicProblem
 
                     activities.Add(userAddition);
 
-                    foreach (string activity activities)
+                    foreach (string activity in activities)
                     {
                         Console.Write($"{activity} ");
                         Thread.Sleep(250);
@@ -55,10 +89,18 @@ ProblematicProblem
 
                     Console.WriteLine();
                     Console.WriteLine("Would you like to add more? yes/no: ");
-                    string addToList = bool.Parse(Console.ReadLine());
+                    var contResponse4 = Console.ReadLine().ToLower();
+                    if (contResponse4 == "yes")
+                    {
+                        addToList = true;
+                    }
+                    else
+                    {
+                        addToList = false;
+                    }
                 }
             }
-            
+
             while (cont)
             {
                 Console.Write("Connecting to the database");
@@ -69,9 +111,9 @@ ProblematicProblem
                     Thread.Sleep(500);
                 }
 
-                Console.WriteLine()
+                Console.WriteLine();
 
-                Console.Write("Choosing your random activity");
+                Console.WriteLine("Choosing your random activity");
 
                 for (int i = 0; i < 9; i++)
                 {
@@ -79,28 +121,29 @@ ProblematicProblem
                     Thread.Sleep(500);
                 }
 
-                Console.WriteLine()
+                Console.WriteLine();
 
                 int randomNumber = rng.Next(activities.Count);
 
-                string randomActivity = activities[randomNumber]
+                string randomActivity = activities[randomNumber];
 
-                if (userAge > 21 && randomActivity == "Wine Tasting")
+
+                if (userAge < 21 && randomActivity == "Wine Tasting")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
 
                     activities.Remove(randomActivity);
 
-                    string randomNumber = rng.Next(activities.Count);
+                    randomNumber = rng.Next(activities.Count);
 
-                    string randomActivity = activities[randomNumber];
+                    randomActivity = activities[randomNumber];
                 }
 
-                Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ")
-                ConsoleWriteLine();
-                bool cont = bool.Parse(Console.ReadLine());
+                Console.WriteLine($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+                Console.WriteLine();
+                cont = (Console.ReadLine().ToLower() == "redo") ? true : false;
             }
         }
     }
-}}
+}
